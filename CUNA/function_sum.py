2585e42169dc4328e14b6f486e6dc9e4fd590486
Nicholas Magi <nicholas.magi24@gmail.com>
2024-04-20 11:18:59 +0200

    updated gitignore

--- CUNA/function_sum.py
+++ CUNA/function_sum.py
@@ -3,23 +3,47 @@ from General import *
 
 class FunctionSum(Scene):
     def construct(self):
-        _A = ValueTracker(0)
+        A = ValueTracker(0)
         omega = ValueTracker(0)
         alpha = ValueTracker(0)
 
-        sin = always_redraw(lambda: classic_plane.plot(lambda x: _A.get_value() * np.sin(omega.get_value() * x + alpha.get_value()), color=RED))
+        ax = Axes(
+            x_range=[-4, 4],
+            y_range=[-5, 5],
+            axis_config={
+                "include_numbers": True})
+
+        def draw_sen_values_label():
+            label = MathTex(f"y = {A.get_value()}\sin({omega.get_value()}x + {alpha.get_value()})", substrings_to_isolate=[f"{A.get_value()}", f"{omega.get_value()}", f"{alpha.get_value()}"])
+            return label
         
-        cos = always_redraw(lambda: classic_plane.plot(lambda x: _A.get_value() * np.cos(omega.get_value() * x + alpha.get_value()), color=GREEN))
+        sin = always_redraw(lambda: ax.plot(lambda x: A.get_value() * np.sin(omega.get_value() * x + alpha.get_value()), color=RED))
+        cos = always_redraw(lambda: ax.plot(lambda x: A.get_value() * np.cos(omega.get_value() * x + alpha.get_value()), color=GREEN))
+
+        fsum = always_redraw(lambda: ax.plot(lambda x: A.get_value() * 
+            (np.sin(omega.get_value() * x + alpha.get_value()) + np.cos(omega.get_value() * x + alpha.get_value())), color=BLUE))
+        
+        sen_values_label = always_redraw(lambda: draw_sen_values_label().move_to(ax.get_origin()).shift(UP * 2 + LEFT * 2.5))
+
+        functions = VGroup(ax, sin, cos, fsum)
+        functions.scale(.75)
 
         self.play(AnimationGroup(
-            Create(classic_plane),
+            Create(ax),
             Create(sin),
             Create(cos),
-            run_time=5
+            Create(fsum),
+            Write(sen_values_label)
         ))
 
-        for i in range(0, 5):
-            self.play(_A.animate.set_value(i ** 2))
+        for i in [1, 2]:
+            self.play(A.animate.set_value(i ** 2))
             self.play(omega.animate.set_value(i * 2))
             self.play(alpha.animate.set_value(i / 2))
-            self.wait(1)
+            self.wait(.3)
+
+        for i in [-1, -2]:
+            self.play(A.animate.set_value(i ** 2))
+            self.play(omega.animate.set_value(i * 2))
+            self.play(alpha.animate.set_value(i / 2))
+            self.wait(.3)
---
+++ Intro_CUNA.py
@@ -0,0 +1,10 @@
+from manim import *
+from General import *
+
+class Intro_CUNA(Scene):
+    def construct(self):
+        self.camera.background_color = BACKGROUND_CLR
+
+        ax = Axes(
+            
+        )
