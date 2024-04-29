1e71ad871547a9d3dd03ed3365b246fdf4354716
Nicholas Magi <nicholas.magi24@gmail.com>
2024-04-30 00:34:47 +0200

    minor changes

--- Source_CUNA/Tetraktys_SOUND.py
+++ Source_CUNA/Tetraktys_SOUND.py
@@ -6,6 +6,9 @@ class Tetraktys_Sound(Scene):
         colors = [MAROON, GREEN, TEAL, ORANGE]
         circles = VGroup()
         
+        def flash_triangle():
+            self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=2)
+
         for i in range(0, 4):
             layer = VGroup()
             for j in range(0, i + 1):
@@ -27,12 +30,12 @@ class Tetraktys_Sound(Scene):
         
         self.play(GrowFromCenter(tetraktys), run_time=7)
         self.wait(.7)
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
-        self.play(AnimationGroup(*[ApplyWave(circle) for circle in circles], run_time=5, lag_ratio=.5))
+        flash_triangle()
+        self.play(AnimationGroup(*[ApplyWave(circle) for circle in circles], lag_ratio=.5))
         self.wait(.7)
         self.play(FadeOut(circles))
 
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
+        flash_triangle()
         
         for i in range(0, 20):
             outer_rnd_index = randint(0, 3)
@@ -46,7 +49,8 @@ class Tetraktys_Sound(Scene):
             #     self.play(Create(circles[outer_rnd_index][inner_rnd_index]), run_time=.1)
             #     outer_idxs.append(outer_rnd_index)
             #     inner_idxs.append(inner_rnd_index)
-            self.play(Create(circles[outer_rnd_index][inner_rnd_index]), run_time=.1)
+
+            self.play(FadeIn(circles[outer_rnd_index][inner_rnd_index]), run_time=.1)
             self.wait(.4)
             self.play(FadeOut(circles[outer_rnd_index][inner_rnd_index]), run_time=.1)
             self.wait(.4)
@@ -56,7 +60,7 @@ class Tetraktys_Sound(Scene):
             for j in range(0, len([*circles[i]])):
                 self.play(FadeIn(circles[i][j]), run_time=.3)
 
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
+        flash_triangle()
 
         self.play(FadeOut(circles[3]), run_time=.3)
         for i in range(0, 27):
@@ -64,7 +68,7 @@ class Tetraktys_Sound(Scene):
             self.wait(.3)
             self.play(FadeOut(circles[3][i % 4]), run_time=.15)
 
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
+        flash_triangle()
 
         self.play(FadeOut(circles[2]), run_time=.3)
         for i in range(0, 20):
@@ -72,7 +76,7 @@ class Tetraktys_Sound(Scene):
             self.wait(.3)
             self.play(FadeOut(circles[2][i % 3]), run_time=.15)
 
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
+        flash_triangle()
 
         self.play(FadeOut(circles[1]), run_time=.3)
         for i in range(0, 15):
@@ -86,14 +90,14 @@ class Tetraktys_Sound(Scene):
             self.wait(.3)
             self.play(FadeOut(circles[0][i % 1]), run_time=.15)
 
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
+        flash_triangle()
 
         for i in range(0, 20):
             self.play(FadeIn(circles[2][i % 3]), FadeIn(circles[0][i % 1]), run_time=.15)
             self.wait(.3)
             self.play(FadeOut(circles[2][i % 3]), FadeOut(circles[0][i % 1]), run_time=.15)
 
-        self.play(ShowPassingFlash(triangle.copy(), time_width=1), run_time=.6)
+        flash_triangle()
 
         for i in range(0, 20):
             self.play(FadeIn(circles[3][i % 4]), FadeIn(circles[1][i % 2]), run_time=.15)
